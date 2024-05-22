7df75f1a4d94f3f0812fba742be87f5d7cd47f95
Vladyslav Zyma <vladzima13@gmail.com>
2024-05-22 11:50:34 +0300

    add sticky header

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@
 const mobileNavBtnEl = document.querySelector('.mobile-nav-btn');
 const navListEl = document.querySelector('.nav__list');
 
+const headerBoxEl = document.querySelector('.header__box');
+const headerBoxHeight = headerBoxEl.getBoundingClientRect().height;
+const sectionHero = document.querySelector('.hero');
+
 // MOBILE NAV
 mobileNavBtnEl.addEventListener('click', function (e) {
   e.stopPropagation();
@@ -10,3 +14,27 @@ mobileNavBtnEl.addEventListener('click', function (e) {
   navListEl.classList.toggle('nav__list--mobile');
   document.body.classList.toggle('_no-scroll');
 });
+
+// STICKY HEADER
+function headerObsCallback(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    headerBoxEl.classList.add('header__box--sticky');
+  } else {
+    headerBoxEl.classList.remove('header__box--sticky');
+  }
+}
+
+const headerObsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerBoxHeight}px`,
+};
+
+const headerObserver = new IntersectionObserver(
+  headerObsCallback,
+  headerObsOptions
+);
+
+headerObserver.observe(sectionHero);
