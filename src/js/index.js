5544f3bd97fcccd9fccec32f1f8d49d50c81cf62
Vladyslav Zyma <vladzima13@gmail.com>
2024-05-22 10:55:56 +0300

    add mobile nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 'use strict';
+const mobileNavBtnEl = document.querySelector('.mobile-nav-btn');
+const navListEl = document.querySelector('.nav__list');
+
+// MOBILE NAV
+mobileNavBtnEl.addEventListener('click', function (e) {
+  e.stopPropagation();
+
+  this.classList.toggle('mobile-nav-btn--active');
+  navListEl.classList.toggle('nav__list--mobile');
+});
